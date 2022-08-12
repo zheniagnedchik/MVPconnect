d2c5ce1ddfccf149713726ad9541b68d4c480779
YK_96 <Kast2406@gmail.com>
2022-08-12 19:20:25 +0300

    fixed burgerMenu

--- src/components/mainComponent/mainPage/burgerMenu/BurgerMenu.jsx
+++ src/components/mainComponent/mainPage/burgerMenu/BurgerMenu.jsx
@@ -1,33 +1,51 @@
 import BurgerMenuCss from './BurgerMenu.module.css';
 import MvpIcon from './img/MvpConnectIcon.svg';
 import BurgerIcon from './img/BurgerIcon.svg';
-import { useRef } from 'react';
+import React from 'react';
 
-const BurgerMenu = (props) => {
-    const checkScroll = useRef();
-    const fixedBurgerMenu = () => {
-        if (
-            (window.scrollY == window.innerHeight &&
-                checkScroll.current.style.position == 'absolute') ||
-            (window.scrollY > window.innerHeight &&
-                checkScroll.current.style.position == 'absolute')
-        ) {
-            checkScroll.current.style.position = 'fixed';
-        } else {
-            checkScroll.current.style.position = 'absolute';
-        }
+class BurgerMenu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            prevScrollpos: window.pageYOffset,
+            pastScrollpos: window.innerHeight,
+            visible: false,
+        };
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll = () => {
+        const { pastScrollpos } = this.state;
+        const currentScrollPos = window.pageYOffset;
+        const visible = currentScrollPos > pastScrollpos;
+        this.setState({
+            prevScrollpos: currentScrollPos,
+            visible,
+        });
     };
-    window.addEventListener('scroll', fixedBurgerMenu);
 
-    return (
-        <div ref={checkScroll} className={BurgerMenuCss.burgerMenu}>
-            <div className={BurgerMenuCss.mvpIcon}>
-                <img src={MvpIcon} alt="" />
+    render() {
+        return (
+            <div
+                className={`${BurgerMenuCss.burgerMenu} ${
+                    this.state.visible ? BurgerMenuCss.fixed : ''
+                }`}
+            >
+                <div className={BurgerMenuCss.mvpIcon}>
+                    <img src={MvpIcon} alt="" />
+                </div>
+                <div className={BurgerMenuCss.burgerIcon}>
+                    <img src={BurgerIcon} alt="" />
+                </div>
             </div>
-            <div className={BurgerMenuCss.burgerIcon}>
-                <img src={BurgerIcon} alt="" />
-            </div>
-        </div>
-    );
-};
+        );
+    }
+}
 export default BurgerMenu;
