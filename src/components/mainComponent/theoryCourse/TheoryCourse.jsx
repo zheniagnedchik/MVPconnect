51f2211c6fc49335f8f107e3b4d8566185d2043f
YK_96 <Kast2406@gmail.com>
2022-08-20 01:32:11 +0300

    install API Splide, added Splide in component TheoryCourse

--- src/components/mainComponent/theoryCourse/TheoryCourse.jsx
+++ src/components/mainComponent/theoryCourse/TheoryCourse.jsx
@@ -1,7 +1,103 @@
 import TheoryCourseCss from './TheoryCourse.module.css';
+import VectorRight from './img/VectorRight.svg';
+import VectorLeft from './img/VectorLeft.svg';
+import slider1 from './img/slider1.svg';
+import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css/skyblue';
 
 const TheoryCourse = () => {
-    return <div className={TheoryCourseCss.theoryCourse}></div>;
+    return (
+        <div className={TheoryCourseCss.theoryCourse}>
+            <div className={TheoryCourseCss.titleWrapper}>
+                <div className={TheoryCourseCss.title}>Теоретические курсы</div>
+                <div className={TheoryCourseCss.subTitle}>
+                    Большие проекты, смелые идеи и новые технологии станут
+                    частью твоей работы!
+                </div>
+            </div>
+            <Splide
+                onActive={(Slide) => {
+                    console.log(Slide);
+                }}
+                hasTrack={false}
+                options={{
+                    classes: {
+                        pagination: `splide__pagination ${TheoryCourseCss.pagination}`,
+                        page: `splide__pagination__page`,
+                    },
+                }}
+                aria-label="..."
+            >
+                <div className={TheoryCourseCss.splideWrapper}>
+                    <SplideTrack>
+                        <SplideSlide>
+                            {' '}
+                            <div className={TheoryCourseCss.slider}>
+                                <div className={TheoryCourseCss.sliderImg}>
+                                    <img src={slider1} alt="" />
+                                </div>
+                                <div className={TheoryCourseCss.sliderTitle}>
+                                    Введение в програмирование
+                                </div>
+                                <div className={TheoryCourseCss.sliderSubTitle}>
+                                    Вы изучите распространенные языки
+                                    програмирования.
+                                </div>
+                                <div className={TheoryCourseCss.sliderBtn}>
+                                    Подробнее
+                                </div>
+                            </div>
+                            <br />
+                        </SplideSlide>
+                        <SplideSlide>
+                            {' '}
+                            <div className={TheoryCourseCss.slider}>
+                                <div className={TheoryCourseCss.sliderImg}>
+                                    <img src={slider1} alt="" />
+                                </div>
+                                <div className={TheoryCourseCss.sliderTitle}>
+                                    Введение в програмирование
+                                </div>
+                                <div className={TheoryCourseCss.sliderSubTitle}>
+                                    Вы изучите распространенные языки
+                                    програмирования.
+                                </div>
+                                <div className={TheoryCourseCss.sliderBtn}>
+                                    Подробнее
+                                </div>
+                            </div>
+                        </SplideSlide>
+                        <SplideSlide>
+                            {' '}
+                            <div className={TheoryCourseCss.slider}>
+                                <div className={TheoryCourseCss.sliderImg}>
+                                    <img src={slider1} alt="" />
+                                </div>
+                                <div className={TheoryCourseCss.sliderTitle}>
+                                    Введение в програмирование
+                                </div>
+                                <div className={TheoryCourseCss.sliderSubTitle}>
+                                    Вы изучите распространенные языки
+                                    програмирования.
+                                </div>
+                                <div className={TheoryCourseCss.sliderBtn}>
+                                    Подробнее
+                                </div>
+                            </div>
+                        </SplideSlide>
+                    </SplideTrack>
+
+                    <div className="splide__arrows" />
+                    <button className="splide__arrow splide__arrow--prev">
+                        <img src={VectorLeft} alt="" />
+                    </button>
+                    <button className="splide__arrow splide__arrow--next">
+                        <img src={VectorRight} alt="" />
+                    </button>
+                </div>
+            </Splide>
+        </div>
+    );
 };
 
 export default TheoryCourse;
